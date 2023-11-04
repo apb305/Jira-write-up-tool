35e11d4695c245fe8667c0b69eeb54e9e4066ec4
Anthony Bernard <25408995+apb305@users.noreply.github.com>
2023-11-04 18:01:44 -0400

    minor updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,72 @@
-function AppNav() {
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+const pages = [
+  { pageName: "Home", pageLink: "/" },
+  { pageName: "Create", pageLink: "/create" },
+  { pageName: "Templates", pageLink: "/#" },
+];
+
+function ResponsiveAppBar() {
   return (
-    <>
-      
-    </>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            JIRA Creator
+          </Typography>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "none" },
+              flexGrow: 1.5,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            JIRA Creator
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { md: "'flex'" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.pageName}
+                href={page.pageLink}
+                sx={{ my: 2, color: "white" }}
+              >
+                {page.pageName}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
-export default AppNav;
+export default ResponsiveAppBar;
